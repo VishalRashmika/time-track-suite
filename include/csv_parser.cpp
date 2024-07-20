dbd0b191e2738389f5f6e43cdd653e3520723833
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-20 10:54:51 +0530

    error fix

--- include/csv_parser.cpp
+++ include/csv_parser.cpp
@@ -1,108 +1,115 @@
-#include <iostream>  
-#include <fstream>  
-#include <string>  
-#include <vector>  
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 #include <sstream>
 
 #include "csv_parser.h"
 
-using namespace std;  
-  
-vector<vector<string>> rows;
+using namespace std;
 
-vector<vector<string>> read_csv(std::string filepath) {    
-//   std::ostringstream address;
-//   address << (void const *)filepath;
-//   std::string something = address.str();
+vector<vector<string>> CSV_parser::read_csv(std::string filepath)
+{
 
-  ifstream infile(filepath);  
-    std::cout << filepath <<std::endl;
-  // Read CSV into rows  
-  string line;
-  int counter;   
-  while(getline(infile, line)) {  
-    vector<string> row;
-    counter = 0;
-    string field;  
-    stringstream ss(line);  
+    // std::string location{"/home/akira/Downloads/letterboxd-akiraarashikage-2024-07-20-04-10-utc/wathched.csv"};
+
+    ifstream infile(filepath);
+    vector<vector<string>> rows;
+
+    // std::cout << "Location : " << location << std::endl;
     
-    while(getline(ss, field, ',')) { 
-        if (counter == 1 || counter == 3){
-            row.push_back(field);
+    // Read CSV into rows
+    string line;
+    int counter;
+    while (getline(infile, line))
+    {
+        vector<string> row;
+        counter = 0;
+        string field;
+        stringstream ss(line);
+
+        while (getline(ss, field, ','))
+        {
+            if (counter == 1 || counter == 3)
+            {
+                row.push_back(field);
+            }
+            counter += 1;
         }
-        counter += 1;
-    }  
-    rows.push_back(row);  
-  }
+        rows.push_back(row);
+    }
 
-    std::cout <<  rows.size() << std::endl;
-    //   std::cout << rows[0][0] << std::endl;
-//   std::cout << rows[0][1] << std::endl;
-    //  std::cout << rows[1][0] << std::endl;
-    //   std::cout << rows[1][1] << std::endl;
+    // std::cout << rows.size() << std::endl;
+    // std::cout << rows[1][0] << std::endl/;
 
-  return rows;
-} 
-
-int main(){
-    std::vector<std::vector<std::string>> data;
-    std::string filepath {"../tests/wathched.csv"};
-    data = read_csv(filepath);
-    return 0;
+    return rows;
 }
 
+// int main()
+// {
+//     std::vector<std::vector<std::string>> data;
+//     std::string filepath{"./watched.csv"};
+//     data = read_csv(filepath);
+//     for (int i = 0; i < data.size(); i++){
+//         for (int j = 0; j < 2; j++){
+//             std::cout << data[i][j] << std::endl;
+//         }
+//     }
+//     return 0;
+// }
+
 /*
-//   // Create a new record  
-//   vector<string> newRecord {"Sam", "35", "Boston"};  
-  
-//   // Add new record      
-//   rows.push_back(newRecord);  
-  
-//   // Write updated CSV  
-//   ofstream outfile("data.csv");  
-  
-//   for(auto row : rows) {  
-//     for(auto field : row) {  
-//       outfile << field << ",";  
-//     }  
-//     outfile << "\n";  
-//   }  
-  
-//   outfile.close();  
+//   // Create a new record
+//   vector<string> newRecord {"Sam", "35", "Boston"};
+
+//   // Add new record
+//   rows.push_back(newRecord);
+
+//   // Write updated CSV
+//   ofstream outfile("data.csv");
+
+//   for(auto row : rows) {
+//     for(auto field : row) {
+//       outfile << field << ",";
+//     }
+//     outfile << "\n";
+//   }
+
+//   outfile.close();
 
 
-#include <iostream>  
-#include <fstream>  
-#include <string>  
-#include <vector>  
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 #include <sstream>
-using namespace std;  
-  
-vector<vector<string>> rows;  
+using namespace std;
 
-int main() {    
-  ifstream infile("../tests/watched.csv");  
-  
-  // Read CSV into rows  
-  string line;   
-  while(getline(infile, line)) {  
-    vector<string> row;  
-      
-    string field;  
-    stringstream ss(line);  
-    
-    while(getline(ss, field, ',')) {  
-      row.push_back(field);  
-    }  
-  
-    rows.push_back(row);  
-  }  
+vector<vector<string>> rows;
+
+int main() {
+  ifstream infile("../tests/watched.csv");
+
+  // Read CSV into rows
+  string line;
+  while(getline(infile, line)) {
+    vector<string> row;
+
+    string field;
+    stringstream ss(line);
+
+    while(getline(ss, field, ',')) {
+      row.push_back(field);
+    }
+
+    rows.push_back(row);
+  }
   std::cout <<  rows.size() << std::endl;
   std::cout << rows[0][3] << std::endl;
   std::cout << rows[1][1] << std::endl;
   std::cout << rows[1][3] << std::endl;
-  
-  return 0;  
-} 
+
+  return 0;
+}
 
 */
