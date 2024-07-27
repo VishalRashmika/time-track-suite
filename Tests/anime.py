f0f625cd68c8e8d75a9ce0bc53b08c69f4494097
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-07-27 11:03:35 +0530

    data extraction

--- Tests/anime.py
+++ Tests/anime.py
@@ -3,18 +3,23 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import json
 
-with open('export.json', 'r') as file:
-    json_data = json.load(file)
+# with open('export.json', 'r') as file:
+#     json_data = json.load(file)
 
-for key, value in json_data.items():
-    # print(key + ':' + str(value))
-    for i in range(0,len(value)):
-        print(value[i]['name'])
+# for key, value in json_data.items():
+#     # print(key + ':' + str(value))
+#     for i in range(0,len(value)):
+#         print(value[i]['name'])
 
+myanimelist = "https://myanimelist.net/anime/49926/"
+anilist = "https://anilist.co/anime/129874/s"
 
+def get_anilist_data(anilist_url):
+    response = requests.get(anilist_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    text = str(soup.find_all(class_="data-set"))
+    # text2 = str(text.find_all(class_="type"))
 
-# url = "helll"
+    return text
 
-# response = requests.get(url)
-# soup = BeautifulSoup(response.content, 'html.parser')
-# text = str(soup.find_all(class_="text-link text-footer"))
+print(get_anilist_data(anilist))
