10b08130a5d4922ff076782683bcbb81d02aa43f
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-07-27 12:17:05 +0530

    json handler

--- Tests/anime.py
+++ Tests/anime.py
@@ -3,13 +3,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import json
 
-# with open('export.json', 'r') as file:
-#     json_data = json.load(file)
-
-# for key, value in json_data.items():
-#     # print(key + ':' + str(value))
-#     for i in range(0,len(value)):
-#         print(value[i]['name'])
 
 myanimelist = "https://myanimelist.net/anime/49926/"
 anilist = "https://anilist.co/anime/129874/s"
@@ -22,17 +15,27 @@ def get_anilist_data(anilist_url):
     text2 = soup2.find_all(class_="value")
     episodes = str(str(text2[1]).split(">")[1]).split("<")[0]
     duration = str(str(text2[2]).split(">")[1]).split(" ")[0]
-    watchtime = int(duration) * int(episodes)
+    runtime = int(duration) * int(episodes)
 
-    return watchtime
+    return runtime
 
 def get_myanimelist_data(my_animelist_url):
     response = requests.get(my_animelist_url)
     soup = BeautifulSoup(response.content, 'html.parser')
     episodes = str(str(soup.find_all(class_="spaceit_pad")[4]).split(" ")[4]).strip("n")
     duration = str(soup.find_all(class_="spaceit_pad")[16]).split(" ")[4]
-    watchtime = int(duration) * int(episodes)
+    runtime = int(duration) * int(episodes)
 
-    return watchtime
+    return runtime
 
-print(get_myanimelist_data(myanimelist))
+def calc_runtime(file_location):
+    total_runtime = 0
+    with open('export.json', 'r') as file:
+        json_data = json.load(file)
+
+        for items in json_data['Watched']:
+            print(f"Name : {items['name']} | AniList : {items['al']} | MyAnimeList : {items['mal']}")
+    file.close()
+    return total_runtime
+
+calc_runtime("d")
